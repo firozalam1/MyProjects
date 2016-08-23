c56947bd62eda913cd5cf1d797d9430606aecd52
firozalam1 <firoz.alam0@gmail.com>
2016-08-23 12:02:03 +0530

    Update Test.java

--- HelloWorld/src/main/java/demo/Test.java
+++ HelloWorld/src/main/java/demo/Test.java
@@ -3,5 +3,6 @@ package demo;
 public class Test{
  public static void main(String[] args){
    System.out.println("Hello world - gradle");
+    System.out.println("Hello world - uploaded in git");
  }
 }
